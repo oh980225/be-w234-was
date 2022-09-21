54d75f938d8fbbff64ea0c6e46821bec604ffb78
george.5 <george.5@kakaocorp.com>
2022-09-21 14:07:59 +0900

    [parameterizedTest 적용]
    * 관련 의존성 추가
    * 기존에 작성되어 있던 테스트 중 일부 parameterizedTest 적용

--- src/test/java/webserver/RequestStartLineTest.java
+++ src/test/java/webserver/RequestStartLineTest.java
@@ -2,22 +2,17 @@ package webserver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RequestStartLineTest {
-    @Test
-    @DisplayName("요청 헤더의 값은 null일 수 없습니다.")
-    void constructor_null() {
-        assertThatThrownBy(() -> new RequestStartLine(null))
-                .isInstanceOf(WebServerException.class)
-                .hasMessage(WebServerErrorMessage.EMPTY_REQUEST.getDetail());
-    }
-
-    @Test
-    @DisplayName("요청 헤더의 값은 빈 문자열일 수 없습니다.")
-    void constructor_empty() {
-        assertThatThrownBy(() -> new RequestStartLine(""))
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("요청 헤더의 값은 null이거나 비어있을 수 없습니다.")
+    void constructor_null_or_empty(String startLine) {
+        assertThatThrownBy(() -> new RequestStartLine(startLine))
                 .isInstanceOf(WebServerException.class)
                 .hasMessage(WebServerErrorMessage.EMPTY_REQUEST.getDetail());
     }
