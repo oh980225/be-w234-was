e58dd9f3c45d294deff9dd6430138248fa15722a
george.5 <george.5@kakaocorp.com>
2022-09-29 17:59:09 +0900

    [ProtocolTest 변경]
    * 좀 더 읽는 사람 입장에서 그 테스트의 의도가 명확하고 직관적의도록 수정

--- src/test/java/webserver/ProtocolTest.java
+++ src/test/java/webserver/ProtocolTest.java
@@ -2,18 +2,15 @@ package webserver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ProtocolTest {
-    @ParameterizedTest
-    @EnumSource(Protocol.class)
-    @DisplayName("Enum으로 제공하고 있는 프로토콜들을 지원하고 있습니다.")
-    void findByValue_HTTP_1_1(Protocol protocol) {
-        assertThat(Protocol.findByName(protocol.getName())).isEqualTo(protocol);
+    @Test
+    @DisplayName("HTTP/1.1을 지원하고 있습니다.")
+    void findByValue_HTTP_1_1() {
+        assertThat(Protocol.findByName("HTTP/1.1")).isEqualTo(Protocol.HTTP_1_1);
     }
 
     @Test
